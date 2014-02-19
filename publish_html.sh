f3a3ae52aad6453e413b369d65beaf21758d486a
Chris P <sahib@online.de>
2014-02-19 21:46:23 +0100

    xxx

--- publish_html.sh
+++ publish_html.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 # This is probably very prone to fail.
-
-function build {
-    git checkout $1
-    make clean html && cp _build/*html /tmp -r;
-    git checkout gh-pages
-    mkdir -p $1 
-    rm *html -rf && cp /tmp/*html $1 -r
-    git add $1
-    git commit -am 'Automated update.'
-    git checkout master
-}
-
-build project
-build bachelor
+git checkout $1
+make clean html singlehtml && cp _build/*html /tmp -r;
+git checkout gh-pages
+rm projekt -rf  
+mkdir -p projekt 
+cp /tmp/*html projekt -r
+git add projekt
+git commit -am 'Automated update.'
+git checkout master
